300278297ea560228f9d72ed890413327f840d4e
MahbubJoy-web <mahbubjoy.web@gmail.com>
2024-10-20 23:43:32 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let asbey_jeta = document.querySelector(".main_input")
+let button = document.querySelector(".Neon")
+let jekane_asbey = document.querySelector(".summa")
+
+button.addEventListener("click" ,()=>{
+    jekane_asbey.innerHTML=asbey_jeta.value
+    
+})
+
+let input = document.querySelector(".main_input")
+let buttom = document.querySelector(".Neon")
+let display = document.querySelector(".tanha")
+
+
+buttom.addEventListener("click" ,()=>{
+    display.innerHTML=input.value
+})
+
+let money= document.querySelector(".Neon")
+let fan = document.querySelector(".main_input")
+let gurer = document.querySelector ('.himu')
+
+money.addEventListener("click" ,()=>{
+    gurer.innerHTML=fan.value
+})
+
+
+let bike= document.querySelector('.joy')
+let cabi=document.querySelector('.Neon')
+let ham=document.querySelector('.main_input')
+
+
+cabi.addEventListener("click" ,()=>{
+    bike.innerHTML=ham.value
+})
+
